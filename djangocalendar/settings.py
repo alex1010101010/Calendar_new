362f32452b557bddc943da84baa946f3d34c0b00
alex1010101010 <64451032+alex1010101010@users.noreply.github.com>
2020-06-05 14:24:13 +1000

    Update settings.py
    'remove pass'

--- djangocalendar/settings.py
+++ djangocalendar/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = '######'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = '#######'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
